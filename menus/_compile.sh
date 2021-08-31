40a8bcc24e27cc65a7efaab5e16edf12262eaf47
Rick Christy <grymmjack@gmail.com>
2021-08-31 03:38:39 -0400

    Getting there - refactor works!

--- menus/_compile.sh
+++ menus/_compile.sh
@@ -1,3 +1,4 @@
+        printf "\n..\n";                                                #     }
 #!/bin/bash
 USER_DIR=$(wslpath "$(env.exe | grep USERPROFILE | cut -d= -f2 | sed $'s/\r//')")
 MENU_FILE="${USER_DIR}/Documents/LES/menuconfig.ini"
@@ -10,33 +11,37 @@ rm -f "${MENU_FILE}"
 {
                                                                         # MENU SIMULATION
 
-    printf "/Instruments\n"                                             # Instruments
-        printf "Instrument Rack\nInstrument Rack\n--\n";                #    Instrument Rack
+    printf "/Instruments\n"                                             # Instruments {
+        printf "Instrument Rack\nInstrument Rack";                      #    Instrument Rack
         printf "\n--\n";                                                #    --
-        cat "Instruments - Ableton.txt";                                #    Ableton Instruments
+        cat "Instruments - Ableton.txt";                                #       Ableton Instruments
         printf "\n--\n";                                                #    --
-        cat "Instruments - User.txt";                                   #    User Instruments
+        cat "Instruments - User.txt";                                   #       User Instruments
+        printf "\n..\n";                                                # }
 
-    printf "\n\n/Drums\n"                                               # Drums
-        printf "Drum Rack\nDrum Rack\n--\n";                            #    Drum Rack
+    printf "\n\n/Drums\n"                                               # Drums {
+        printf "Drum Rack\nDrum Rack";                                  #    Drum Rack
         printf "\n--\n";                                                #    --
-        cat "Drums - Ableton.txt";                                      #    Ableton Drums
+        cat "Drums - Ableton.txt";                                      #       Ableton Drums
         printf "\n--\n";                                                #    --
-        cat "Drums - User.txt";                                         #    User Drums
+        cat "Drums - User.txt";                                         #       User Drums
+        printf "\n..\n";                                                # }
 
-    printf "\n\nMIDI Effects\n";                                        # MIDI Effects
-        printf "MIDI Effect Rack\nMIDI Effect Rack\n--\n";              #    MIDI Effect Rack
+    printf "\n\n/MIDI Effects\n";                                       # MIDI Effects {
+        printf "MIDI Effect Rack\nMIDI Effect Rack";                    #    MIDI Effect Rack
         printf "\n--\n";                                                #    --
-        cat "MIDI Effects - Ableton.txt";                               #    Ableton MIDI Effects
+        cat "MIDI Effects - Ableton.txt";                               #       Ableton MIDI Effects
         printf "\n--\n";                                                #    --
-        cat "MIDI Effects - User.txt";                                  #    User MIDI Effects
+        cat "MIDI Effects - User.txt";                                  #       User MIDI Effects
+        printf "\n..\n";                                                # }
 
-    printf "\n\nAudio Effects\n";                                       # Audio Effects
-        printf "Audio Effect Rack\nAudio Effect Rack\n--\n";            #     Audio Effect Rack
+    printf "\n\n/Audio Effects\n";                                      # Audio Effects {
+        printf "Audio Effect Rack\nAudio Effect Rack";                  #     Audio Effect Rack
         printf "\n--\n";                                                #     --
-        cat "Audio Effects - Ableton.txt";                              #     Ableton Audio Effects
+        cat "Audio Effects - Ableton.txt";                              #       Ableton Audio Effects
         printf "\n--\n";                                                #     --
-        cat "Audio Effects - User.txt";                                 #     User Audio Effects
+        cat "Audio Effects - User.txt";                                 #       User Audio Effects
+        printf "\n..\n";                                                # }
 
     printf "\nEnd\n";                                                   # End marker - REQUIRED!
 
